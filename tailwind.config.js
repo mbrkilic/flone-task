809d56d187fa15980244b6883c52b4eed2294aae
unknown <mdburakkilic@gmail.com>
2023-06-26 14:55:51 +0300

    flone task

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+      lowBlack: '#313131'
+  },
+  fontFamily: {
+    poppins: [ 'Poppins', "sans-serif"]
+  }},
+  },
+  plugins: [],
+}
+
