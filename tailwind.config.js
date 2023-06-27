cee70c9ec50c2492807464fb645da5dd50a9e142
unknown <mdburakkilic@gmail.com>
2023-06-27 15:46:07 +0300

    responsive design sec

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     extend: {
       colors: {
       lowBlack: '#313131'
